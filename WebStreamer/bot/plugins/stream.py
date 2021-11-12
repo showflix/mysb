399d5863b405d1db84025d5a92e93f1ed728c744
Show Flix <83741010+showflix@users.noreply.github.com>
2021-11-12 17:19:03 +0530

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -36,8 +36,9 @@ async def media_receive_handler(_, m: Message):
     stream_link = Var.URL  + str(log_msg.message_id) + '/' +quote_plus(file_name) if file_name else ''
     response= requests.get(BASE_URL+stream_link)  
     final_sb_url =STREAMSB_URL+response.json().get("result").get("filecode")+".html" 
+    file_name=file_name.replace("@"," ").replace("."," ").replace("_"," ").replace("-"," ")
     print(file_name)
-    response2= requests.get(DROP_URL+final_sb_url)
+    response2= requests.get(DROP_URL+final_sb_url+"&alias="+file_name)
     final_drop_url=response2.json().get("shortenedUrl")
     print(final_drop_url)
     await m.reply_text(
