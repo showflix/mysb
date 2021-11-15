5f86e9d3b8dbe81a57a734254242aefd856ccd94
Show Flix <83741010+showflix@users.noreply.github.com>
2021-11-15 17:58:50 +0530

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -13,7 +13,7 @@ API_S= Var.API_SB
 BASE_URL="https://api.streamsb.com/api/upload/url?key="+API_S+"&url="
 STREAMSB_URL="https://embedsb.com/"
 DROP_URL="https://droplink.co/api?api=d61f38da5372ed4c13655f94ee3564b20f6fd525&url="
-
+GP_LINK="https://gplinks.in/api?api=ceb30468694ff94f00f40127cee2c2223d39d42a&url="
 def detect_type(m: Message):
     if m.document:
         return m.document
@@ -39,9 +39,11 @@ async def media_receive_handler(_, m: Message):
     
     file_name_api=file_name.replace("@","").replace(".","").replace("_","").replace("-","").replace(" ","").replace("x","")
     
-    response2= requests.get(DROP_URL+final_sb_url+"&alias=file"+str(log_msg.message_id))
+    response2= requests.get(DROP_URL+final_sb_url+"&alias=showflixfile"+str(log_msg.message_id))
+    response3= requests.get(GP_LINK+final_sb_url+"&alias=showflixfile"+str(log_msg.message_id))
     
     final_drop_url=response2.json().get("shortenedUrl")
+    final_gp_link=response3.json().get("shortenedUrl")
     
     file_name=file_name.replace("@"," ").replace("."," ").replace("_"," ").replace("-"," ")
     
@@ -49,7 +51,7 @@ async def media_receive_handler(_, m: Message):
     
     
     await m.reply_text(
-        text=f"<b>Movie Name: </b>```{file_name}```\n\n" f"<b>Movie Link:</b> {final_drop_url}\n\n" f"📥 Uploaded by : ██▓▒░⡷⠂𝚂𝙷𝙾𝚆𝙵𝙻𝙸𝚇⠐⢾░▒▓██\n\n",
+        text=f"<b>Movie Name: </b>```{file_name}```\n\n" f"<b>Movie Link 1: </b> {final_drop_url}\n\n" f"<b>Movie Link 2: </b> {final_gp_link}\n\n" f"<b>📤 Uploaded by :</b> ██▓▒░⡷⠂𝚂𝙷𝙾𝚆𝙵𝙻𝙸𝚇⠐⢾░▒▓██\n\n" f"<b>📥 How to Download: </b> https://t.me/tamilmoviereqst/19897 \n\n" f"<b>Join us :</b> @tvshowsmoviesonline",
         quote=True,
         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('Join Our Group', url="https://telegram.me/tamilmoviereqst")]])
       
